6eb7d04fe99144da8c18afce7c4175fb976a2f2f
ccQpein <ccqpein@protonmail.com>
2016-02-23 17:44:15 -0500

    fix the database bugs

--- applib/dbStore.py
+++ applib/dbStore.py
@@ -12,18 +12,19 @@ else:
     sqlite3.connect(sys.path[0] + '/accoutingRecords.db')
     conn = sqlite3.connect(sys.path[0] + '/accoutingRecords.db')
     with conn:
-        conn.execute('''create table accountingList (object_Name char() not null,
-                                                object_Price varchar(77) not null,
-                                                    primary key(object_Name))
-                    ''')
+        conn.execute('''create table accountingList (object_Name varchar(77) not null,
+                                                object_Price float(77) not null);''')
 
 
 def do_sql(strr):
     with conn:
-        cur = conn.execute(strr)
-    return cur
+        conn.execute(strr)
 
 
-def add_sql(name, price):
-    do_sql('''insert into accountingList value ("{objectname}", "{objectprice}");'''.format(
-        objectname=name, objectprice=prince))
+def add_sql(thename, price):
+    strr = '''insert into accountingList values("{objectname}", "{objectprice}");'''.format(
+        objectname=thename, objectprice=price)
+    do_sql(strr)
+
+if __name__ == "__main__":
+    add_sql("test", 100)
