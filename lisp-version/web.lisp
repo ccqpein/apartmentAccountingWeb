b14ed1c9165a65d9822da46f9b24a085b1020ecf
ccQpein <ccqpein@protonmail.com>
2016-11-07 10:08:56 -0500

    test clack web packages

--- lisp-version/web.lisp
+++ lisp-version/web.lisp
@@ -1,2 +1,8 @@
 (load "~/quicklisp/setup.lisp")
 (ql:quickload :clack)
+
+(defvar *handler*
+    (clack:clackup
+      (lambda (env)
+        (declare (ignore env))
+        '(200 (:content-type "text/plain") ("Hello, Clack!")))))
