5da6bc6ed688ed61fdd2b5ad1bdf03c39a758fc9
ccQpein <ccqpein@protonmail.com>
2016-11-20 17:30:14 -0500

    a bit

--- PHP-version/index.php
+++ PHP-version/index.php
@@ -1,4 +1,16 @@
 <?php
+$session_life_time = 60 * 60;
+session_set_cookie_params($lifetime, '/');
+session_start();
+
+if (empty($_SESSION["temp"])) {
+    $_SESSION["temp"] = array();
+    $user_data = filter_input(INPUT_POST,"user_data",
+                          FILTER_SANITIZE_SPECIAL_CHARS, FILTER_REQUIRE_ARRAY);
+}else{
+    $user_data = $_SESSION["user_data"];
+}
+
 require("./model/database.php");
 
 
